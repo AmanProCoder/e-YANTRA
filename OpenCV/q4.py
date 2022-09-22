47f2a94dc729ab23da45b77d5b850be183e8255f
Aman <amanprocoder@gmail.com>
2022-09-22 15:25:20 +0530

    modified the ques4

--- OpenCV/q4.py
+++ OpenCV/q4.py
@@ -1,9 +1,23 @@
-t=int(input())
-l=[]
-for i in range(t):
-    a=list(map(int,input().split()))
-    l.append(a)
-for i in l:
-    print("%0.2f"%((i[0]-i[1])**2+(i[2]-i[3]**2)))
+from math import dist
+def compute_distance(x1, y1, x2, y2):
+    # distance = "%0.2f"%(math.sqrt(((x1-x2)**2+(y1-y2)**2)))
+    p=[x1,y1]
+    q=[x2,y2]
+    distance="%0.2f"%(dist(p,q))
+    return distance
+    
 
     
+
+if __name__ == '__main__':
+    l=[]
+    test_cases = int(input())
+    for i in range(test_cases):
+        a=list(map(int,input().split()))
+        l.append(a)
+    for i in l:
+        x1=i[0]
+        y1=i[1]
+        x2=i[2]
+        y2=i[3]
+        print(compute_distance(x1, y1, x2, y2))
